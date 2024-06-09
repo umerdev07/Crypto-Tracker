7a8c58adce33223391163805b2f77afba4c6897b
M.Umer Yousaf <zonerespect2023@gmail.com>
2024-06-09 22:26:46 +0500

    Initial Commet

--- app/src/main/java/com/maths/cryptotracker/splashscreen.kt
+++ app/src/main/java/com/maths/cryptotracker/splashscreen.kt
@@ -1,6 +1,7 @@
 package com.maths.cryptotracker
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -15,11 +16,33 @@ class splashscreen : AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN)
         supportActionBar?.hide()
         setContentView(R.layout.activity_splashscreen)
+        HeavyTask();
 
-        Handler(Looper.getMainLooper()).postDelayed({
-             val intent = Intent(this , MainActivity::class.java)
+    }
+
+    private fun HeavyTask() {
+       LongOperation().execute()
+    }
+    private open inner class LongOperation : AsyncTask<String? , Void? , String?>(){
+        override fun doInBackground(vararg params: String?): String? {
+
+            var i = 0
+            while (i<=6){
+                try{
+                    Thread.sleep(1000)
+                }catch (e:Exception){
+                    Thread.interrupted()
+                }
+                i++
+            }
+            return "result"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val intent = Intent(this@splashscreen,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }
     }
 }
